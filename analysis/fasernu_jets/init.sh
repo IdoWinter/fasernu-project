5e2173bb7edd8329ac6953f199dabc4d30e4bd0c
Ido Winter <ido.winter@gmail.com>
2024-11-28 20:19:19 +0200

    Fix init.sh

--- analysis/fasernu_jets/init.sh
+++ analysis/fasernu_jets/init.sh
@@ -1,6 +1,6 @@
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
-setupATLAS
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 lsetup "root 6.32.06-x86_64-el9-gcc13-opt"
 export VDT_INCLUDE_DIR=/cvmfs/sft.cern.ch/lcg/releases/vdt/0.4.4-48c41/x86_64-el9-gcc13-opt/include
 export FASTJET_DIR=/cvmfs/sft.cern.ch/lcg/releases/fastjet/3.4.1-5af57/x86_64-el9-gcc13-opt
